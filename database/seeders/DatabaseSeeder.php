80da7061059bd1603227fbce0ae8540bb162c902
BartoszJS <bartosz290300@wp.pl>
2022-06-01 00:07:03 +0200

    2 12%

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,5 +24,6 @@ class DatabaseSeeder extends Seeder
         // ]);
         Company::factory()->count(10)->create();
         Contact::factory()->count(50)->create();
+        Company::factory()->hasContacts(5)->count(10)->create();
     }
 }
