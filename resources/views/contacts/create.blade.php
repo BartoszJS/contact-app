072de442a85f3d17de167f3dc71975da9672caec
BartoszJS <bartosz290300@wp.pl>
2022-06-01 00:56:35 +0200

    12 15%

---
+++ resources/views/contacts/create.blade.php
@@ -0,0 +1,3 @@
+<h1>Add new</h1>
+
+<a href="{{ route('contacts.index') }}">Back to all</a>
---
+++ resources/views/contacts/show.blade.php
@@ -0,0 +1,7 @@
+<h1>Contact details</h1>
+
+<div>
+    {{$contact}}
+</div>
+
+<a href="{{route('contacts.index')}}">Back to all</a>
